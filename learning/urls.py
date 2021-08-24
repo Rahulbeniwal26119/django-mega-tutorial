6ad76cdad3c6d1859b3b3b304427956b7857d46c
Rahul Beniwal <rahul.b@lendenclub.com>
2021-08-24 08:14:00 +0530

    adding urls for polls

--- learning/urls.py
+++ learning/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("decorators.urls"))
+    path('', include("decorators.urls")),
+    path('polls/', include("polls.urls"))
 ]
