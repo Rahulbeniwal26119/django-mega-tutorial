8a3b980968f3660760fbbd930544dcb691b85d27
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-09-13 08:01:25 +0530

    customizing admins panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+
 from .models import Question, Choice
+
+
 # from .models import
 # Register your models here.
-admin.site.register([Question, Choice])
+
+class QuestionAdmin(admin.ModelAdmin):
+    # create custom field sets
+    fieldsets = [
+        ("Question Text", {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]})
+    ]
+    # fields = [f for f in Question().__dict__.keys() if f not in ["id", "_state"]]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register([Choice])
