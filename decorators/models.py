adf1315de494635790b46f6ba6a20dcb2ad3a7eb
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-12-17 08:42:15 +0530

    adding models for faculty and authorization

--- decorators/models.py
+++ decorators/models.py
@@ -7,3 +7,33 @@ class Loan(models.Model):
     amount = models.FloatField(null=True, default=0)
     number = models.CharField(max_length=10)
 
+class Result(models.Model):
+    name = models.CharField(max_length=100)
+    percentage = models.FloatField()
+
+    class Meta:
+        db_table = 'result'
+        verbose_name = 'Result'
+        verbose_name_plural = 'Results'
+
+Faculty_Choices = (
+    ("TEACHING", "TEACHING"),
+    ("NON-TEACHING", "NON-TEACHING"),
+)
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+    faculty_id = models.CharField(max_length=10)
+    faculty_type = models.CharField(max_length=100, choices = Faculty_Choices)
+
+    class Meta:
+        db_table = 'faculty'
+        verbose_name = 'Faculty'
+        verbose_name_plural = 'Faculties'
+class AuthToken(models.Model):
+    key = models.TextField()
+    user = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'auth_token'
+        verbose_name = 'Auth Token'
+        verbose_name_plural = 'Auth Tokens'
