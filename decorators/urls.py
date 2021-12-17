1213ebc1fc37ab03606e47566038571339f80304
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-12-17 08:43:16 +0530

    adding url for result

--- decorators/urls.py
+++ decorators/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import print_query
+from .views import print_query, get_result
 
 urlpatterns = [
-    path("loan/<int:pk>", print_query, name="Print Query")
+    path("loan/<int:pk>", print_query, name="Print Query"),
+    path("result/", get_result, name="get_result"),
 ]
